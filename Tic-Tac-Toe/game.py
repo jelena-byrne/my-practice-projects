d7dd731123c6a565c7c23087e137647c470c3b29
jelena-byrne <jelena.byrne@gmail.com>
2022-02-06 20:29:45 -0600

    updated files

---
+++ Tic-Tac-Toe/game.py
@@ -0,0 +1,34 @@
+class TicTacToe:
+    def __init__(self) -> None:
+        # we will ue a single list to represent 3x3 board
+        self.board = [' ' for _ in range(9)]
+        self.current_winner = None  # keep track of winner!
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('| ' + ' | '.join(row)+' |')
+
+    @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2 etc (tells us what number corresponds to what box)
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)]
+                        for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row)+' |')
+
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == ' ']
+        # moves = []
+        # for (i,x) in enumerate(self.board):
+        #     # ['x', 'x', 'o'] --> [(0, 'x'), (1, 'x'), (2, 'o')]
+        #     if spot == ' ':
+        #         moves.append(i)
+        # return moves
+
+
+def play(game, x_player, o_player, print_game=True):
+    if print_game:
+        game.print_board_nums()
+
+    letter - 'X'  # starting letter
+    # continue here...
--- Tic-Tac-Toe/tic-tac-toe=main.py
+++ Tic-Tac-Toe/tic-tac-toe=main.py
@@ -1,2 +1,44 @@
 import math
 import random
+from re import L
+
+
+class Player:
+    def __init__(self, letter):
+        # letter is X or O
+        self.letter = letter
+
+    # we want all players to get their next move given a game
+    def get_move(self, game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            # check for a correct value of input by casting it to an integer
+            # if it's not an integer, we will say the move is invalid
+            # if the spot is not available on the board, we will say invalid move
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True  # if these are successful, great!
+            except ValueError:
+                print('Invalid square. Try again.')
+        return val
